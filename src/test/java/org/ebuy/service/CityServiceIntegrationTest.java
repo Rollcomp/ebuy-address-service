6aa7509b280433f5341d10b3f8e2b5c205c397db
Burak Koken <burakkokenn@gmail.com>
2020-05-14 01:34:07 +0200

    Removed import for power mockito.

--- src/test/java/org/ebuy/service/CityServiceIntegrationTest.java
+++ src/test/java/org/ebuy/service/CityServiceIntegrationTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import java.util.ArrayList;
 import java.util.List;
